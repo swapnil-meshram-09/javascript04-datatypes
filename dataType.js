2fea2972ab230fcfd590b01ee3cc6819de88c482
swapnil-meshram-09 <swapnilmeshram514@gmail.com>
2025-12-17 16:22:19 +0530

    new commit

---
+++ dataType.js
@@ -0,0 +1,53 @@
+// premitive data types
+
+character = 'c';
+string = 'string';    // string
+integer = 123;        // number
+float = 123.444;
+bool1 = true;          // boolean
+bool2 = false;
+value = null;          // object
+value2 = undefined;    // undefined
+
+
+
+
+// references 
+
+array = [1,2,3,4,5];                // object
+mixedArray = [1,'a','fdg'];         // object
+objects = {
+    name:"myName",                  // object
+    rollNo : 123
+}
+
+
+
+console.log(character);
+console.log(string);
+console.log(integer);
+console.log(float);
+console.log(bool1);
+console.log(bool2);
+console.log(value);
+console.log(value2);
+console.log(array);
+console.log(mixedArray);
+console.log(objects);
+
+
+console.log("\ntypes\n");
+
+console.log(typeof(character));
+console.log(typeof(string));
+console.log(typeof(integer));
+console.log(typeof(float));
+console.log(typeof(bool1));
+console.log(typeof(bool2));
+console.log(typeof(value));
+console.log(typeof(value2));
+console.log(typeof(array));
+console.log(typeof(mixedArray));
+console.log(typeof(objects));
+
+
